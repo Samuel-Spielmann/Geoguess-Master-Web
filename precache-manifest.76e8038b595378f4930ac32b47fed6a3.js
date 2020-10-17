d7b33ab721508e622193cc054a14b90822778426
SamuelSpielmann <sspielmann@babbel.com>
2020-10-17 16:45:55 +0200

    deploy

---
+++ precache-manifest.76e8038b595378f4930ac32b47fed6a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Geoguess-Master-Web/robots.txt"
+  },
+  {
+    "revision": "4939aa91015851504ec2",
+    "url": "/Geoguess-Master-Web/js/chunk-vendors.307ef1c1.js"
+  },
+  {
+    "revision": "17a1aa38cd7a244aec1b",
+    "url": "/Geoguess-Master-Web/js/app.f23430a2.js"
+  },
+  {
+    "revision": "cba1ee88d52e731c58b8a612d82d2454",
+    "url": "/Geoguess-Master-Web/index.html"
+  },
+  {
+    "revision": "8d57d4a139f9fac6d6c598435d957d10",
+    "url": "/Geoguess-Master-Web/img/logo.8d57d4a1.png"
+  },
+  {
+    "revision": "255fa5890bff55ae77d29bbaff941ee2",
+    "url": "/Geoguess-Master-Web/img/earth.255fa589.jpg"
+  },
+  {
+    "revision": "4939aa91015851504ec2",
+    "url": "/Geoguess-Master-Web/css/chunk-vendors.da5dd690.css"
+  },
+  {
+    "revision": "17a1aa38cd7a244aec1b",
+    "url": "/Geoguess-Master-Web/css/app.ed84ee91.css"
+  },
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/Geoguess-Master-Web/_redirects"
+  }
+];
